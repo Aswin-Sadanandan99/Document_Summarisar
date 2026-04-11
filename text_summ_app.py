f72225fc7956716a458b9bd626dc33f04c9170f6
Aswin Sadanandan <aswinsadanandan99@gmail.com>
2026-04-11 14:14:16 +0530

    Update Hugging Face token and format code
    Updated Hugging Face token and ensured code formatting.

--- text_summ_app.py
+++ text_summ_app.py
@@ -5,7 +5,7 @@ from pypdf import PdfReader
 
 # ------------------- CONFIG -------------------
 API_URL = "https://router.huggingface.co/hf-inference/models/sshleifer/distilbart-cnn-12-6"
-HF_TOKEN = "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"  # <-- put your token here
+HF_TOKEN = "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"  # <-- put your token here
 
 headers = {
     "Authorization": f"Bearer {HF_TOKEN}",
